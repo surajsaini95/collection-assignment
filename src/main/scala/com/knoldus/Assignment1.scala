6826a242ba5b67b0d93e3499fd680bc9c0f5f357
surajsaini95 <surajkumar90@rocketmail.com>
2020-02-06 14:04:53 +0530

    solution2-added

---
+++ src/main/scala/com/knoldus/Assignment1.scala
@@ -0,0 +1,32 @@
+package com.knoldus
+
+class Assignment1 {
+  def secondMaxFromList(list : List[Int]) : Int = {
+    1
+  }
+  def kthFromList(list : List[Int],k : Int) : Int = {
+    1
+  }
+  def isListPalindrome(list : List[Int]) : Boolean = {
+    true
+  }
+  def reverseList(list : List[Int]) : List[Int] = {
+
+  }
+  def firstEvenFromList(list : List[Int]) : Int = {
+    1
+  }
+  def compressList(list : List[Int]) : List[Int] = {
+
+  }
+  def duplicateList(list : List[Int]) : List[Int] = {
+
+  }
+  def dropFromList(element :Int,list : List[Int]) : List[Int] = {
+
+  }
+
+}
+object Assignment1 extends App{
+
+}
